59b3ac3d5ab7fd3fc10a608d54822f18658527f3
SnoopyVIBES <ananyakngl@gmail.com>
2025-02-25 18:55:27 -0800

    div point to html

---
+++ script.js
@@ -0,0 +1,20 @@
+window.setInterval(changeSlide, 5000); 
+let currentSlide = 0;
+
+function changeSlide() {
+  let slides = document.querySelectorAll(".slide");
+
+  let lastSlide = currentSlide;
+  currentSlide++;
+
+  if (lastSlide < 0) {
+    lastSlide = slides.length - 1;
+  }
+  if (currentSlide >= slides.length) {
+    currentSlide = 0;
+  }
+  slides[lastSlide].classList.toggle("show");
+  slides[currentSlide].classList.toggle("show");
+
+  // hi gurl annika bigback //
+}
